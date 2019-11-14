92bc52f83b5f0932e9bdd94dbd27b6beb20bde4b
Matti Hirvonen / Microteam <matti.hirvonen@comatec.fi>
2019-11-14 12:06:57 +0200

    Build script detect compiling environment

--- build.sh
+++ build.sh
@@ -1,29 +1,46 @@
 #!/bin/bash
 
-CC=qcc
+QCC='qcc -Vgcc_ntoarmv7le'
+GCC='gcc'
 
-TGT=-V
+if [ $OSTYPE == msys ]
+then
+    CC=$QCC
+else
+    CC=$GCC
+fi
+
 
 #  getopt.c  lib_sched.c  lib_stats.c   lib_debug.c  -lm
 
 do_compile() {
 
     cd   src
-#   $CC  -DHAVE_socklen_t  -o $1  $1.c  getopt.c  lib_timing.c  lib_sched.c
-    $CC  -DHAVE_socklen_t  -o $1  $1.c  getopt.c
+    $CC  -DHAVE_socklen_t  -o $1  $1.c  getopt.c  PIPE.C  lib_timing.c  lib_sched.c  lib_mem.c
+#   $CC  -DHAVE_socklen_t  -o $1  $1.c  getopt.c
     mv   $1  ..
     cd   ..
 }
 
-build_msgring() {
+do_single() {
     cd   src
-    $CC  -DHAVE_socklen_t  -o $1  $1.c
+    $CC  -DHAVE_socklen_t -o $1  $1.c
     mv   $1  ..
     cd   ..
 }
 
+ do_single   msgring
 #do_compile  hello
-#do_compile  lat_pipe
-#do_compile  lat_ctx
 
- build_msgring  msgring
+ do_compile  lat_pipe
+ do_compile  lat_ctx
+ do_compile  lat_ctx_qnx
+
+ do_compile  cache
+ do_compile  bw_mem
+ do_compile  bw_file_rd
+ do_compile  lat_mem_rd
+ do_compile  lat_dram_page
+
+ do_compile  bw_mmap_rd
+ do_compile  lat_pagefault
