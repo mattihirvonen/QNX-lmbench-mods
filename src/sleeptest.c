d9aa2fdea4dca764c35ae3e79f7208ecce14c575
Matti Hirvonen / Comatec <matti.hirvonen@comatec.fi>
2020-10-17 10:33:09 +0300

    Add "-d" command line switch to "sleeptest" and fix result print

--- src/sleeptest.c
+++ src/sleeptest.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>       // strcmp()
 #include <unistd.h>       // sleep()
 #include <time.h>
 #include <sys/time.h>
@@ -23,28 +24,38 @@ int main( int argc, char *argv[] )
     struct timespec  tp_start, tp_end, tp_diff;
     struct timeval   tv_start, tv_end, tv_diff;
 
-    int sleeptime = 10;
-    int command   = atoi( argv[1] );
-
-    if (command)
-        sleeptime = command;
-
-    printf("Sleep test %d seconds...\n", sleeptime);
-
     clock_gettime( CLOCK_MONOTONIC, &tp_start );
     gettimeofday( &tv_start, NULL );
 
+    int  i, decimals = 0, sleeptime = 10;
+
+    for (i = 1; i < argc; i++) {
+
+        int delay = atoi( argv[i] );
+        if (delay)
+            sleeptime = delay;
+
+        if (!strcmp("-d",argv[i]))
+            decimals = 1;
+    }
+    printf("Sleep test %d seconds...\n", sleeptime);
+
     sleep( sleeptime );
+
+    printf("...done\n\n");
 
     clock_gettime( CLOCK_MONOTONIC, &tp_end );
     gettimeofday( &tv_end, NULL );
-
     diff_tp( &tp_diff, &tp_start, &tp_end );
     diff_tv( &tv_diff, &tv_start, &tv_end );
 
-    printf("...done\n");
-    printf("\n");
     printf("Results:\n");
-    printf("- clock_gettime: %3d.%06d seconds\n", (int)tp_diff.tv_sec, (int)(tp_diff.tv_nsec/1000000) );
-    printf("- gettimeofday:  %3d.%06d seconds\n", (int)tv_diff.tv_sec, (int)(tv_diff.tv_usec/1000)    );
+    if (decimals) {
+        printf("- clock_gettime: %3d.%09d seconds\n",    (int)tp_diff.tv_sec, (int)(tp_diff.tv_nsec) );
+        printf("- gettimeofday:  %3d.%06d    seconds\n", (int)tv_diff.tv_sec, (int)(tv_diff.tv_usec) );
+    }
+    else {
+        printf("- clock_gettime: %3d.%03d seconds\n", (int)tp_diff.tv_sec, (int)(tp_diff.tv_nsec/1000000) );
+        printf("- gettimeofday:  %3d.%03d seconds\n", (int)tv_diff.tv_sec, (int)(tv_diff.tv_usec/1000)    );
+    }
 }
